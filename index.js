e145cedb3fe9228c2c3b49c37c28c6b7abee87a3
Irungaray <lionelirungaray@gmail.com>
2020-12-26 09:45:54 -0300

    Linted and fixed

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 'use strict'
 
-require('dotenv').config();
+require('dotenv').config()
 
-const { graphql, buildSchema } = require('graphql');
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
 
-const app = express();
+const app = express()
 const port = process.env.PORT || 3000
 
 console.log(
   port === process.env.PORT
-  ? 'Server running on env var port'
-  : 'Server running on default port'
-);
+    ? 'Server running on env var port'
+    : 'Server running on default port'
+)
 
 // Defining Schema
 const schema = buildSchema(`
   type Query {
     "Returns a Hello World"
     hello: String
+
     "Returns una reputeada"
     greetings: String
   }
-`);
+`)
 
 // Configure Resolvers
 const resolvers = {
@@ -34,14 +35,15 @@ const resolvers = {
   greetings: () => {
     return 'Returns una reputeada'
   }
-};
+}
 
+// Defining Server
 app.use('/api', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
   graphiql: true
-}));
+}))
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}/api`)
-});
+})
